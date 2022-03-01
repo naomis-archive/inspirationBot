194e5628cfc0058daf774f5fa2cf6ce288c831d5
saira <69580485+saira512dev@users.noreply.github.com>
2022-03-01 23:35:27 +0530

    Update QuoteList.ts
    I have added a few quotes.I will be happy to add more

--- src/config/QuoteList.ts
+++ src/config/QuoteList.ts
@@ -17,4 +17,56 @@ export const QuoteList: Quote[] = [
     text: "Believe you can and you’re halfway there.",
     author: "Theodore Roosevelt",
   },
+  {
+    text: "The biggest room in the world is room for improvement. ",
+    author: "Helmut Schmidt",
+  },
+  {
+    text: "Kind words do not cost much. Yet they accomplish much. ",
+    author: "Blaise Pascal",
+  },
+   {
+    text: "Nothing is a waste of time if you use the experience wisely",
+    author:"Rodin",
+  },
+   {
+    text: "The most beautiful things in the world cannot be seen or even touched. They must be felt with the heart",
+    author: "Helen Keller",
+  },
+   {
+    text: "Give me six hours to chop down a tree and I will spend the first four sharpening the axe",
+    author: "Abraham Lincoln",
+  },
+   {
+    text: "Enjoy the little things, for one day you may look back and realize they were the big things.",
+    author: "Robert Brault",
+  },
+   {
+    text: "If you don't go after what you want, you'll never have it. If you don't ask, the answer is always no. If you don't step forward, you're always in the same place.",
+    author: "Nora Roberts",
+  },
+   {
+    text: "All men who have achieved great things have been great dreamers",
+    author: "Orison Marden",
+  },
+   {
+    text: "Forgiveness does not change the past, but it does enlarge the future",
+    author: "Paul Boese",
+  },
+   {
+    text: "Who looks outside, dreams; who looks inside, awakes",
+    author: "Carl Jung",
+  },
+   {
+    text: "The only person who never makes mistakes is the person who never does anything.",
+    author: "Denis Waitley",
+  },
+   {
+    text: " If you don't know where you are going, you will probably end up somewhere else",
+    author: "Lawrence Peter",
+  },
+   {
+    text: "Courage is going from failure to failure without losing enthusiasm",
+    author: "Winston Churchill",
+  },
 ];
