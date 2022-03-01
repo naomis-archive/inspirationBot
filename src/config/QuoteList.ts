761f0b1581eca84de1e29de1dd04d34f045bb847
Isaac Oyedele <82805750+izzyben20@users.noreply.github.com>
2022-03-01 23:54:23 +0100

    feat: added quotes by Barack Obama, Mae West & Robert Frost (#22)
    * added quotes by Barack Obama, Mae West & Robert Frost
    
    * added a space
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>

--- src/config/QuoteList.ts
+++ src/config/QuoteList.ts
@@ -102,6 +102,22 @@ export const QuoteList: Quote[] = [
     author: "Winston Churchill",
   },
   {
+    text: "If you're walking down the right path and you're willing to keep walking, eventually you'll make progress.",
+    author: "Barack Obama",
+  },
+  {
+    text: "Where you are right now doesn't have to determine where you'll end up.",
+    author: "Barack Obama",
+  },
+  {
+    text: "You only live once, but if you do it right, once is enough",
+    author: "Mae West",
+  },
+  {
+    text: "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost",
+  },
+  {
     text: "A really great man is known by three signs... generosity in the design, humanity in the execution, and moderation in success.",
     author: "Otto Von Bismarck",
   },
