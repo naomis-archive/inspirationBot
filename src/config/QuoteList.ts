f0fc955d4a5bd1e1f8e41ed2152fdd9454f80a0d
Naomi Carrigan <nhcarrigan@gmail.com>
2022-03-02 09:35:25 -0800

    fix: my fault with the rebase

--- src/config/QuoteList.ts
+++ src/config/QuoteList.ts
@@ -182,7 +182,6 @@ export const QuoteList: Quote[] = [
     author: "Dale Carnegie",
   },
   {
-
     text: "The truth is you don’t know what is going to happen tomorrow. Life is a crazy ride, and nothing is guaranteed.",
     author: "Eminem",
   },
